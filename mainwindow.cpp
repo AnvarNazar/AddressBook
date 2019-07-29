b04c204c18f4583c8eaa75a4dc7ec9219c854137
Anvar Nazar <anvarnasar@ymail.com>
2019-07-29 13:47:06 +0530

    ui created

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(AddressBookController *controller, QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , _controller(controller)
 {
     ui->setupUi(this);
+
+    setupConnections();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::createEntry()
+{
+    auto entry = _controller->createEntry();
+    if (entry) {
+        ui->listWidget->addItem(entry->name());
+    }
+}
+
+void MainWindow::setupConnections()
+{
+    connect(ui->actionAdd, SIGNAL(triggered(bool)),
+            this, SLOT(createEntry()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <addressbookcontroller.h>
+
 #include <QMainWindow>
 
 namespace Ui {
@@ -12,11 +14,18 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(AddressBookController *controller, QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+
+    void createEntry();
 
 private:
     Ui::MainWindow *ui;
+    AddressBookController *_controller;
+
+    void setupConnections();
 };
 
 #endif // MAINWINDOW_H
